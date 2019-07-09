2ec289e430b047de4bc7327bc9ae35a708f10284
Biing-yann Hsiung (bear) <bhsiung@linkedin.com>
2019-07-09 00:29:49 -0700

    static basic component

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,4 +1,12 @@
 import Route from '@ember/routing/route';
 
 export default class IndexRoute extends Route {
+  async model() {
+    return {
+      clockConfigs: [
+        { name: 'high intense' },
+        { name: 'cardio' }
+      ]
+    }
+  }
 }
