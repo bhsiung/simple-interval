8b300537c3aa537b9945c6a35f5f80c8a8ae04c9
Biing-yann Hsiung (bear) <bhsiung@linkedin.com>
2019-07-19 01:06:13 -0700

    add fonts

--- app/reducers/clocks.js
+++ app/reducers/clocks.js
@@ -1,6 +1,6 @@
 const initState = [
-  { name: 'high intense', round: 4, timeOn: 15000, timeOff: 30000 },
-  { name: 'cardio', round: 8, timeOn: 20000, timeOff: 10000 },
+  { name: 'HIGH INTENSE', round: 4, timeOn: 15000, timeOff: 30000 },
+  { name: 'CARDIO', round: 8, timeOn: 20000, timeOff: 10000 },
   { name: '5x5', round: 5, timeOn: 20000, timeOff: 30000 }
 ];
 
