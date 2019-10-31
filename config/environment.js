227e1898f9ccac06807fd5c689fa3cb798bae747
Biing-yann Hsiung (bear) <bhsiung@linkedin.com>
2019-10-30 18:53:29 -0700

    update css & remove fastboot

--- config/environment.js
+++ config/environment.js
@@ -21,9 +21,6 @@ module.exports = function(environment) {
     APP: {
       // Here you can pass flags/options to your application instance
       // when it is created
-    },
-    fastboot: {
-      hostWhitelist: ['bhsiung-mn1', /^localhost:\d+$/]
     }
   };
 
