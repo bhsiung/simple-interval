6cdc56625ddd077c593bede686be1405c41c02f9
Biing-yann Hsiung (bear) <bhsiung@linkedin.com>
2020-01-02 01:59:26 -0800

    enable local storage
    - also minor style fine tune for mobile

--- app/reducers/clocks.js
+++ app/reducers/clocks.js
@@ -1,13 +1,15 @@
 const initState = [
-  { name: 'HIGH INTENSE', round: 4, timeOn: 15000, timeOff: 30000 },
+  { name: 'INTENSIVE', round: 4, timeOn: 15000, timeOff: 30000 },
   { name: 'CARDIO', round: 8, timeOn: 20000, timeOff: 10000 },
   { name: '5x5', round: 5, timeOn: 20000, timeOff: 30000 }
 ];
+const cachedInitStateJSON = localStorage.getItem('clockConfig');
+const cachedInitState = cachedInitStateJSON && JSON.parse(cachedInitStateJSON);
 
-export default function clocks(state = initState, action) {
+export default function clocks(state = cachedInitState || initState, action) {
   switch(action.type) {
-    case 'updateClock':
-      return state.map((clock, index) => {
+    case 'updateClock': {
+      const newClocks = state.map((clock, index) => {
         if (index === action.clockId) {
           return {
             name: action.name,
@@ -18,6 +20,9 @@ export default function clocks(state = initState, action) {
         }
         return clock
       })
+      localStorage.setItem('clockConfig', JSON.stringify(newClocks))
+      return newClocks
+    }
     default:
       return state
   }
