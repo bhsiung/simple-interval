4f5c91857f30842b431bdd8a8946b07f50975729
bear <bear2029@gmail.com>
2020-05-17 21:16:20 +0000

    onboard page title

--- app/routes/timer/index.js
+++ app/routes/timer/index.js
@@ -1,9 +1,12 @@
+import { inject as service } from '@ember/service';
 import Route from '@ember/routing/route';
 
 export default class TimerIndexRoute extends Route {
+  @service redux
   model() {
     const { id } = this.paramsFor('timer')
+    const { timers: timerConfigs } = this.redux.store.getState()
 
-    return { id };
+    return { id, title: timerConfigs[id].name };
   }
 }
