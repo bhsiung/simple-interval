19f64525cac3363c9a6f897008de2f59881d2157
bear <bear2029@gmail.com>
2020-05-18 09:15:53 +0000

    onboard ember-cli-meta-tags

--- app/routes/application.js
+++ app/routes/application.js
@@ -1,6 +1,14 @@
 import Route from '@ember/routing/route';
 
 export default class ApplicationRoute extends Route {
+  headTags = [{
+    type: 'meta',
+    tagId: 'meta-description-tag',
+    attrs: {
+      name: 'description',
+      content: 'Free workout timer, 100% customizable for your HIIT or TABATA routine'
+    }
+  }]
   activate() {
     if (!window.addEventListener) return
     window.addEventListener('beforeinstallprompt', () => {
--- app/routes/timer/edit.js
+++ app/routes/timer/edit.js
@@ -3,6 +3,14 @@ import { inject as service } from '@ember/service';
 
 export default class TimerEditRoute extends Route {
   @service redux
+  headTags = [{
+    type: 'meta',
+    tagId: 'meta-description-tag',
+    attrs: {
+      name: 'description',
+      content: 'You can customize the timer here by adjust the number of rounds, and durations'
+    }
+  }]
   model() {
     const { id } = this.paramsFor('timer')
     const timerConfig = this.redux.getState().timers[id]
